76fe96eb5c6b8bd1716248e616ab0134844066f0
Diego Freitas dos Santos <diegofreitas50@hotmail.com>
2022-08-11 21:16:35 -0300

    Feat: myAccount

--- src/User/users.controller.ts
+++ src/User/users.controller.ts
@@ -8,11 +8,11 @@ import { User } from './entities/user.entity';
 import { LoggedUser } from 'src/auth/logged-user.decorator';
 
 @ApiTags('Users')
-@Controller('users')
+@Controller()
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Post('create')
+  @Post('/login/register')
   @ApiOperation({ summary: 'Criar um usuário' })
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
@@ -25,15 +25,26 @@ export class UsersController {
 
   @UseGuards(AuthGuard())
   @ApiBearerAuth()
-  @Get(':id')
+  @Get('/ranking/user/:id')
   @ApiOperation({ summary: 'Visualizar um usuário pelo Id.' })
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(id);
   }
+  
+  @UseGuards(AuthGuard())
+  
+  @ApiBearerAuth()
+  @Get('/myAccount')
+  @ApiOperation({
+    summary: 'Retorna o perfil do usuário autenticado no momento',
+  })
+  profile(@LoggedUser() user: User) {
+    return this.usersService.profile(user);
+  }
 
   @UseGuards(AuthGuard())
   @ApiBearerAuth()
-  @Patch(':id')
+  @Patch('/myAccount')
   @ApiOperation({ summary: 'Editar dados do usuário logado' })
   update(@LoggedUser() user: User, @Body() updateUserDto: UpdateUserDto) {
     updateUserDto.id = user.id;
@@ -43,7 +54,7 @@ export class UsersController {
   @UseGuards(AuthGuard())
   @ApiBearerAuth()
   @HttpCode(HttpStatus.NO_CONTENT)
-  @Delete(':id')
+  @Delete('/myAccount')
   @ApiOperation({
     summary: 'Remove usuário logado.',
   })
