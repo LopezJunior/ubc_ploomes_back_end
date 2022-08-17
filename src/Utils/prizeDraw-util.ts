06d7c491defc3b5335962c57ad0cf1116735f30f
Vinhas93 <vinhas.daniel@gmail.com>
2022-08-17 07:55:55 -0300

    fix: Refactor PrizeDraw function

--- src/Utils/prizeDraw-util.ts
+++ src/Utils/prizeDraw-util.ts
@@ -1,13 +1,16 @@
+// Função para criação de lista de sorteio. 
+// Recebe o limite de rodadas definido na criação da Room
+
+
 export function PrizeDraw(limitPrizeDraw: number) {
   const drawList = [];
   for (let i = 0; i < limitPrizeDraw; i++) {
-    const ball = Math.ceil(Math.random() * 75);
+    const ball = Math.ceil(Math.random() * 75);     // Gerar número aleatório de 1 a 75
 
-    if (drawList.indexOf(ball) != -1) {
+    if (drawList.indexOf(ball) != -1) {             // Caso o número exista na lista, sorteia de novo
       i--;
-      continue;
     } else {
-      drawList.push(ball);
+      drawList.push(ball);                          // Caso seja um novo número, adiciona à lista
     }
   }
   return drawList;
