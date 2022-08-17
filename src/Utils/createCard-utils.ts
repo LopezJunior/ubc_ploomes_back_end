b0bd7f7672c6f5e37dc2009655e8307488271e3a
Felipe <97033128+Felipe360flp@users.noreply.github.com>
2022-08-16 21:36:34 -0300

    fix createCard Function

--- src/Utils/createCard-utils.ts
+++ src/Utils/createCard-utils.ts
@@ -1,61 +1,84 @@
 //Função de criação de cartelas;
 //sorteio de 5 listas (B, I, N, G, O) e organização numa lista maior, 
-//sem armazenar números repetidos.
+//Não armazenar números repetidos.
 
+export function CreateCard(bingo:number[]) {
 
-    let b=[4],i = [4],n = [4],g= [4] ,o = [4];
-    let bingo = [];
-    
-    for(let x=0;x<4;x++){
-        b[x] = Math.floor(Math.random() * (15 - 1) + 1);
+    let b=[],i = [],n = [],g= [] ,o = [];
+
+    for(let x=0;x<5;x++){
+        let y = Math.ceil(Math.random() * 15);
+        if(b.indexOf(y) != -1){
+            x--
+            continue;
+        }else{
+            b.push(y);
+        }
     };
 
-    for(let x=0;x<4;x++){
-        i[x] = Math.floor(Math.random() * (30 - 16) + 16);
+    for(let x=0;x<5;x++){
+        let y = Math.ceil(Math.random() * 15 + 15);
+        if(i.indexOf(y) != -1){
+            x--
+            continue
+        }else{
+            i.push(y);
+        }
     };
 
-    for(let x=0;x<4;x++){
-        n[x] = Math.floor(Math.random() * (45 - 31) + 31);
+
+    for(let x=0;x<5;x++){
+        let y = Math.ceil(Math.random() * 15 + 30);
+        if(n.indexOf(y) != -1){
+            x--
+            continue
+        }else{
+            n.push(y);
+        }
     };
 
-    for(let x=0;x<4;x++){
-        g[x] =  Math.floor(Math.random() * (60 - 46) + 46);
+    for(let x=0;x<5;x++){
+        let y = Math.ceil(Math.random() * 15 + 45);
+        if(g.indexOf(y) != -1){
+             x--
+            continue
+        }else{
+            g.push(y);
+        }
     };
 
-    for(let x=0;x<4;x++){
-        o[x] = Math.floor(Math.random() * (75 - 61) + 61);
+    for(let x=0;x<5;x++){
+        let y = Math.ceil(Math.random() * 15 + 60);
+        if(o.indexOf(y) != -1){
+            x--
+            continue
+        }else{
+            o.push(y);
+        }
     };
 
-    for(let x=0;x<4;x++){
-        bingo[x] = b[x];
-    };
+    for(let x=0;x<5;x++){
+        bingo.push(b[x])
+    }
 
-    for(let x=4;x<8;x++){
-        bingo[x] = i[x];
-    };
+    for(let x=0;x<5;x++){
+        bingo.push(i[x])
+    }
 
-    for(let x=8;x<12;x++){
-        bingo[x] = n[x];
-    };
+    for(let x=0;x<5;x++){
+        bingo.push(n[x])
+    }
 
-    for(let x=12;x<16;x++){
-        bingo[x] = g[x];
-    };
+    for(let x=0;x<5;x++){
+        bingo.push(g[x])
+    }
 
-    for(let x=16;x<20;x++){
-        bingo[x] = o[x];
-    };
+    for(let x=0;x<5;x++){
+        bingo.push(o[x])
+    }
 
-    console.log(`b = ${b}`);
-    console.log(`i = ${i}`);
-    console.log(`n = ${n}`);
-    console.log(`g = ${g}`);
-    console.log(`o = ${o}`);
-    console.log('-------------------------');
-    console.log(bingo);
-
-
-
+    return bingo;
+}
 
 
 
