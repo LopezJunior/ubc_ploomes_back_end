3115f8bfd9e462a643c4c034e4c70c5ea4c66c3f
Felipe <97033128+Felipe360flp@users.noreply.github.com>
2022-08-17 20:21:11 -0300

    Feat Card Dto

--- src/card/dto/create-card.dto.ts
+++ src/card/dto/create-card.dto.ts
@@ -1,1 +1,17 @@
-export class CreateCardDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsString } from 'class-validator';
+
+export class CreateCardDto {
+    @IsNumber()
+    @ApiProperty({
+        description: 'Recebe um vetor numérico, correspondente a cartela do bingo',
+    })
+    vetor: number[];
+    
+    
+    @IsString()
+    @ApiProperty({
+        description: 'Id do proprietário da cartela',
+    })
+     userID: string;
+}
