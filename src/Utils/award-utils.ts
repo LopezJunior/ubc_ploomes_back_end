b0060469b9b42b41bf46a0b5fa9dd383d825073d
Felipe <97033128+Felipe360flp@users.noreply.github.com>
2022-08-18 18:41:29 -0300

    Update award-utils.ts

--- src/Utils/award-utils.ts
+++ src/Utils/award-utils.ts
@@ -2,50 +2,47 @@ import { User } from "src/User/entities/user.entity";
 import { Room } from "src/Room/entities/room-entity";
 import { Prisma } from '@prisma/client';
 import { PrismaService } from "src/prisma/prisma.service";
-import { type } from "os";
+import { UpdateUserDto } from "src/User/dto/update-user.dto";
 
 export async function Award(user:User,room:Room){
-
-    //Geral
+    
     const countUsers = room.users.length; // numero de jogadores
     const userIdList = room.users; // id de todos os usuários
-    let totalCards = 0; // numero total de cards
+    const userPayCards = user.cards.length * room.price;// Pagamento cartelas
+    let totalCards = 0;
 
-    // Todos os cards da Room
+    // Para contar todos os cards da Room
     for(let x=0 ; x<countUsers ; x++){
         let recordUser = await this.PrismaService.user.findUnique({where:{id:userIdList[0]}});
         totalCards += recordUser.cards.length; 
     }
-    
-    //solo
-    //const data: Partial<User> = { ...updateUserDto };
-    //Multiplayer
 
+    const data: Partial<User> = { ...UpdateUserDto};
+   
     if(countUsers<2){
-        await this.PrismaService.user.update({
-            where: {
-              id:user.id,
-            },
-            user:{
-                wallet: user.wallet + 50 - (user.cards.length * room.price)
-            }
+      const userPayCards = user.cards.length * room.price;
 
-          });
+      await this.PrismaService.user.update({
+        where: {
+          id:user.id,
+        },
+        data:{
+          wallet: user.wallet + 50 - userPayCards
+        }
+      });
 
     }else{
 
-        await this.PrismaService.user.update({
-            where: {
-              id:user.id,
-            },
-            user:{
-                wallet: user.wallet + totalCards * countUsers
-            }
+     await this.PrismaService.user.update({
+      where: {
+        id:user.id,
+      },
+      data:{
+        wallet: user.wallet + (totalCards * room.price)
+      }
+    });
 
-          });
-
-
-    }
+  }
 }
 
     
