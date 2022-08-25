53f93811e2e9952aafad62e139de8a59c7c5a772
Daniel Vinhas <vinhas.daniel@gmail.com>
2022-08-24 21:04:20 -0300

    feat: intall websocket dependency

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,11 @@ import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './User/users.module';
 import { RoomModule } from './Room/room.module';
 import { CardModule } from './card/card.module';
+import { AppGateway } from './app.gateway';
 
 @Module({
   imports: [UsersModule, AuthModule, PrismaModule, RoomModule, CardModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AppGateway],
 })
 export class AppModule {}
