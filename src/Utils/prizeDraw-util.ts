b47b7995491484515b67a3df89ac2281316bcbbe
Daniel Vinhas <vinhas.daniel@gmail.com>
2022-09-01 21:28:43 -0300

    ctrl+y

--- src/Utils/prizeDraw-util.ts
+++ src/Utils/prizeDraw-util.ts
@@ -2,7 +2,7 @@
 // Recebe o limite de rodadas definido na criação da Room
 
 export function PrizeDraw(limitPrizeDraw: number): number[] {
-  const prizeOrder = [];
+  const prizeOrder = [0];
   for (let i = 0; i < limitPrizeDraw; i++) {
     const ball = Math.ceil(Math.random() * 75); // Gerar número aleatório de 1 a 75
 
