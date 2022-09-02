02c07cae3b4a312291f569ff229a31e09efd7656
Daniel Vinhas <vinhas.daniel@gmail.com>
2022-09-01 21:38:56 -0300

    fix: refactor commit

--- src/Utils/crossMap-util.ts
+++ src/Utils/crossMap-util.ts
@@ -1,5 +1,5 @@
 // Função que mapeia, na cartela, os índices dos números marcados corretamente e retorna uma lista com estes índices.
-// Recebe lista de números da cartela (card) e lista dos números marcados já verificados (prizeNumbers)
+// Recebe lista de números da cartela (cardNumbers) e lista dos números marcados já verificados (prizeNumbers)
 // Objetivo da função é gerar uma lista que facilite a validação de bingo
 
 export function CrossMap(
